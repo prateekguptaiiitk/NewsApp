0052075c65db844c457ebddb6b3f996de5f2ff4c
Prateek Gupta <prateekjpg@gmail.com>
2020-06-29 18:38:49 +0530

    Update model with new WebView

--- src/component/modal.js
+++ src/component/modal.js
@@ -1,7 +1,7 @@
 //import libraries
 import React, { Component } from 'react';
-import { Dimensions, Modal, Share } from 'react-native';
-import {Container, Header, Content, Body, Left, Icon, Right, Title, Button} from 'native-base';
+import { Dimensions, Modal, Share, ScrollView } from 'react-native';
+import {Container, Header,  Body, Left, Icon, Right, Title, Button} from 'native-base';
 import { WebView } from 'react-native-webview';
 
 const webViewHeight = Dimensions.get('window').height - 56;
@@ -30,38 +30,39 @@ class ModalComponent extends Component {
         const { showModal, articleData } = this.props;
         const { url } = articleData;
         if( url != undefined ) {
-        return (
-            <Modal
-                animationType="slide"
-                transparent
-                visible={showModal}
-                onRequestClose={this.handleClose}
-            >
-                <Container style={{margin:15, marginBottom:0, backgroundColor:'#fff'}}>
-                    <Header style={{backgroundColor:'#009387'}}>
-                        <Left>
-                            <Button onPress={this.handleClose} transparent>
-                                <Icon name="close" style={{color: 'white', fontSize: 12}}/>
-                            </Button>
-                        </Left>
-                        <Body>
-                            <Title children={articleData.title} style={{color: 'white'}}/>
-                        </Body>
-                        <Right>
-                            <Button onPress={this.handleShare} transparent>
-                                <Icon name="share" style={{color: 'white', fontSize: 12}}/>
-                            </Button>
-                        </Right>
-                    </Header>
-                    <Content contentContainerStyle={{height: webViewHeight}}>
-                        <WebView source={{uri:url}} style={{flex: 1}}
-                        onError={this.handleClose} startInLoadingState
-                        scalesPageToFit
-                        />
-                    </Content>
-                </Container>
-            </Modal>
-        );
+            return (
+                <Modal
+                    animationType="slide"
+                    transparent
+                    visible={showModal}
+                    onRequestClose={this.handleClose}
+                >
+                    <Container style={{margin:15, marginBottom:0, backgroundColor:'#fff'}}>
+                        <Header style={{backgroundColor:'#009387'}}>
+                            <Left>
+                                <Button onPress={this.handleClose} transparent>
+                                    <Icon name="close" style={{color: 'white', fontSize: 12}}/>
+                                </Button>
+                            </Left>
+                            <Body>
+                                <ScrollView>
+                                <Title children={articleData.title} style={{color: 'white'}}/>
+                                </ScrollView>
+                            </Body>
+                            <Right>
+                                <Button onPress={this.handleShare} transparent>
+                                    <Icon name="share" style={{color: 'white', fontSize: 12}}/>
+                                </Button>
+                            </Right>
+                        </Header>
+                        <ScrollView contentContainerStyle={{flexGrow: 1}}>
+                            <WebView source={{uri:url}} style={{height: 100}}
+                            onError={this.handleClose} startInLoadingState={true}
+                            />
+                        </ScrollView>
+                    </Container>
+                </Modal>
+            );
         } else {
             return null;
         }
